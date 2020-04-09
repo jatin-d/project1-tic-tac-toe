4fd0ca8dc1556e45e7bdd3c880d27b2f68430b90
Jatin D <b4jatin@gmail.com>
2020-04-09 12:24:47 +1000

    Working model of the game is ready with score counter and reset functions, stylr modification pending

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 console.log('Tic-Tac-Toe');
 
 let boxes = document.querySelectorAll('.box');
+let player1Score = document.querySelector('.pOne');
+let player2Score = document.querySelector('.pTwo');
+let result = document.querySelector('.result');
+let reset = document.querySelector('.reset');
+
+
 let counter = 0;
-const playerOneSelections = [];
-const playerTwoSelections = [];
-const playerOneCombo = [];
-const playerTwoCombo = [];
-let result1 = 0;
-let result2 = 0;
+let playerOneSelections = [];
+let playerTwoSelections = [];
+let playerOneCombo = [];
+let playerTwoCombo = [];
+let hasWinner = false;
+let playerOneScore = 0;
+let playerTwoScore = 0;
 const winningCombinations = [
     ['1', '2', '3'],
     ['4', '5', '6'],
@@ -20,53 +27,71 @@ const winningCombinations = [
 ];
 
 const doCross = (e)=>{
-    // if (result1 === 0 && result2 === 0){
-        e.target.classList.toggle('crossDone');
-        playerOneSelections.push(e.target.classList[0]);
-        createCombo(playerOneSelections, playerOneCombo);
-        // validation(playerOneCombo, winningCombinations, result1);
-        for(let i=0; i<playerOneCombo.length; i++){
-            for(let j=0; j<winningCombinations.length; j++){
-                if(playerOneCombo[i] == winningCombinations[j].sort().join('')){
-                    result1 = result1 +1;
-                }
+    e.target.textContent = 'x'
+    e.target.classList.toggle('crossDone');
+    e.target.classList.toggle('xDone');
+    playerOneSelections.push(e.target.classList[0]);
+    createCombo(playerOneSelections, playerOneCombo);
+    // validation(playerOneCombo, winningCombinations, result1);
+    for(let i=0; i<playerOneCombo.length; i++){
+        for(let j=0; j<winningCombinations.length; j++){
+            if(playerOneCombo[i] == winningCombinations[j].sort().join('')){
+                playerOneScore = playerOneScore + 1;
+                hasWinner = true;
+                break;
             }
         }
-        if(result1 === 1){window.alert("Player 1 has won the match")}
-        counter++;
-    // }
+        if(hasWinner){
+            break;
+        }
+    }
+    counter++;
+    if(hasWinner){
+        result.textContent = "Player 1 wins";
+        player1Score.textContent = playerOneScore;
+    }
 }
 
 const doCircle = (e)=>{
-    // if(result2 === 0 && result1 === 0){
+        e.target.textContent = 'o';
         e.target.classList.toggle('circleDone');
+        e.target.classList.toggle('oDone');
         playerTwoSelections.push(e.target.classList[0]);
         createCombo(playerTwoSelections, playerTwoCombo);
         // validation(playerTwoCombo, winningCombinations, result2);
         for(let i=0; i<playerTwoCombo.length; i++){
             for(let j=0; j<winningCombinations.length; j++){
                 if(playerTwoCombo[i] == winningCombinations[j].sort().join('')){
-                    result2 = result2 +1;
+                    playerTwoScore = playerTwoScore +1;
+                    hasWinner = true;
+                    break;
                 }
             }
+            if(hasWinner){
+                break;
+            }
         }
-        if(result2 === 1){window.alert("Player 2 has won the match")}
         counter++;
-    // }
+        if(hasWinner){
+            result.textContent = "Player 2 wins";
+            player2Score.textContent = playerTwoScore;
+        }
 }
 
 const playGame = (e)=>{
-    if(!e.target.classList.contains('circleDone') && !e.target.classList.contains('crossDone') && result2 === 0 && result1 === 0){
+    if(!e.target.classList.contains('circleDone') && !e.target.classList.contains('crossDone') && hasWinner === false){
         if(counter%2 === 0) {
             doCross(e);
+            // validation(playerOneCombo, winningCombinations, result1);
         } else {
             doCircle(e);
+            // validation(playerTwoCombo, winningCombinations, result2);
         }
-    }
+    } 
 }
 
 const createCombo = (arr1, arr2)=>{
-    for(let i=0; i<arr1.length; i++){ 
+    for(let i=0; i<arr1.length; i++){
         arr2[i]=arr1.slice(i, i+3);
     }
     if(arr2.length > 2){
@@ -79,20 +104,29 @@ const createCombo = (arr1, arr2)=>{
     }
 }
 
+// function createCombo(inputArr){
+//     let resultArr = [];
+//     for(let i = 0; i< inputArr.length; i++){
+//         resultArr.push(inputArr.slice(0,3));
+//         let currentElem = inputArr[0];
+//         inputArr = inputArr.slice(1,inputArr.length);
+//         inputArr.push(currentElem);
+//     }
+//     return resultArr;
+// }
 // const validation =(arr1, arr2, res)=>{
 // for(let i=0; i<arr1.length; i++){
 //     for(let j=0; j<arr2.length; j++){
 //         if(arr1[i] == arr2[j].sort().join('')){
 //             res = res +1;
+//              break;
 //         }
+//     }
+//     if(res === 1){
+//         break
 //     }
 // }
 // }
-
-
-
-
-
 
 
 // const finalValidation = (arr1, arr2, res)=>{
@@ -114,25 +148,23 @@ const createCombo = (arr1, arr2)=>{
 //         return true;
 //     }
 // }
-// let validateScore = (winning, combo, res)=>{
-//     if(combo.length > 2){
-//         for(let i=0; i<winning.length; i++){
-//             for(let j=0; j<combo.length; j++){
-//                 for(k=0; k<winning[i].length; k++){
-//                     for(l=0; l<combo[j].length; l++){
-//                         if(winning[i][k]==combo[j][l]){
-//                             res = res +1;
-//                         }
-//                     }
-//                 }
-
-//             }
-//         }
-//     }
-// }
-
-// console.log(result);
+const resetTheGame = (e)=>{
+    boxes.forEach((element)=>{
+        element.classList.remove('circleDone');
+        element.classList.remove('crossDone');
+        element.classList.remove('xDone');
+        element.classList.remove('oDone');
+        element.textContent = '?';
+        hasWinner = false;
+        counter = 0;
+        playerOneSelections = [];
+        playerTwoSelections = [];
+        playerOneCombo = [];
+        playerTwoCombo = [];
+    })
+}
 
 
 
 boxes.forEach((element)=>{element.addEventListener('click',playGame)});
+reset.addEventListener('click', resetTheGame);
